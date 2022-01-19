b6ed6a722685c91c661a7eff5bc00309482c7ebb
Valloice <vinhs1605@gmail.com>
2022-01-18 16:40:35 -1200

    attempt#1

--- server.js
+++ server.js
@@ -19,15 +19,17 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  let language = req.headers['accept-language']
+  let ip = req.ip
+  let user_agent = req.headers['user-agent']
+  res.json({ipaddress: ip, language: language, software: user_agent})
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT||3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
